215588bc784c46000719814cb69053d49ec11bc0
Vitor Braga <vitor.braga@tomtom.com>
2020-10-09 17:11:15 +0200

    Adjusting scripts paths

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "games-ecommerce-server",
-        script: "./build/src/index.js",
+        script: "./build/index.js",
         instances: "2",
         env: {
             NODE_ENV: "development",
